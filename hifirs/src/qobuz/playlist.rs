29e4ea113f611ad68eee27289b57a497740bf0d2
David Benjamin <db@iamdb.co>
2023-08-29 15:41:37 -0700

    fix error

--- hifirs/src/qobuz/playlist.rs
+++ hifirs/src/qobuz/playlist.rs
@@ -11,7 +11,7 @@ pub struct Playlist {
     pub duration_seconds: usize,
     pub tracks_count: usize,
     pub id: usize,
-    pub cover_art: String,
+    pub cover_art: Option<String>,
     pub tracks: VecDeque<Track>,
 }
 
@@ -27,12 +27,20 @@ impl From<QobuzPlaylist> for Playlist {
             VecDeque::new()
         };
 
+        let cover_art = if let Some(image) = value.image_rectangle.first() {
+            Some(image.clone())
+        } else if let Some(images) = value.images300 {
+            images.first().cloned()
+        } else {
+            None
+        };
+
         Self {
             id: value.id as usize,
             title: value.name,
             duration_seconds: value.duration as usize,
             tracks_count: value.tracks_count as usize,
-            cover_art: value.image_rectangle.first().unwrap().clone(),
+            cover_art,
             tracks,
         }
     }
